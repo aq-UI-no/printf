86a4f4c1a7ff19274d401eb315680ff75a038092
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-14 11:17:33 -0800

    my printf project

---
+++ README.md
@@ -0,0 +1,1 @@
+ This is a repo for my printf project. I have decided to go solo on the project
---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _printf - replicates the printf function
+ *
+ */
+int _printf(const char *format, ...)
